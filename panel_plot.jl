42a6483c48f3cb58d28053b370d709861429db9d
Tobias Slade-Harajda <tobias.slade-harajda@warwick.ac.uk>
2024-01-30 09:22:58 +0000

    first test panel plot

--- panel_plot.jl
+++ panel_plot.jl
@@ -1,6 +1,5 @@
 
 using ArgParse
-
 const argsettings = ArgParseSettings()
 
 @add_arg_table! argsettings begin
@@ -65,296 +64,19 @@ using Distributed, Dates
 using Plots, Random, ImageFiltering, Statistics
 using Dierckx, Contour, JLD2, DelimitedFiles
 
-
-println("Starting at ", now())
-# pitchanglecosine of 0 is all vperp, and (-)1 is (anti-)parallel to B field
-# pitch angle is defined as vpara/v0
-const pitchanglecosine = parsedargs["pitch"]
-@assert -1 <= pitchanglecosine <= 1
-# energy of minority particle and temperature
-const _Emin = parsedargs["minorityenergyMeV"]
-const _Te = parsedargs["temperaturekeV"] # temperature (in keV) of the electrons
-# thermal width of ring as a fraction of its speed # Dendy PRL 1993
-const vthermalfractionz = parsedargs["vthpararatio"]
-const vthermalfraction⊥ = parsedargs["vthperpratio"]
-const _kparamax = parsedargs["kparamax"]
-const _kperpmax = parsedargs["kperpmax"]
-const _ngridpoints = parsedargs["ngridpoints"]
-# concentrations
-const xi2 = parsedargs["secondfuelionconcentrationratio"]
-const ximin = parsedargs["minorityConcentration"]
-# density and magnetic field
-const _B0 = parsedargs["magneticField"]
-const _n0 = parsedargs["electronDensity"]
 # name of file
+# loop through each simulation
 const name_extension = parsedargs["nameextension"]
-const dirpath = mapreduce(i->"_$(i[2])", *, parsedargs; init="run")
+const dirpath = mapreduce(i->"_$(i[2])", *, parsedargs; init="/home/space/phrmsf/Documents/ICE_DS/JET26148/default_params_with_Triton_concentration/run")
 @show dirpath
 const filecontents = [i for i in readlines(open(@__FILE__))]
 const nprocsadded = div(Sys.CPU_THREADS, 2)
-
-addprocs(nprocsadded, exeflags="--project")
 
 @everywhere using ProgressMeter # for some reason must be up here on its own
 @everywhere using StaticArrays
 @everywhere using FastClosures
 @everywhere using NLsolve
-@everywhere begin
-  using LinearMaxwellVlasov, LinearAlgebra, WindingNelderMead
 
-  # mass ratios
-  const mp = 1836.2
-  const md = 3671.5
-  const mT = 5497.93
-  const mHe3 = 5497.885
-  const mα = 7294.3
-
-  # electron mass and charge
-  mₑ = LinearMaxwellVlasov.mₑ
-  ze = -1
-
-  # mass and charge of ions
-#  # D-T-α
-#  # masses
-#  m1 = md*mₑ
-#  m2 = mT*mₑ
-#  mmin = mα*mₑ
-#  # charge numbers
-#  z1 = 1
-#  z2 = 1
-#  zmin = 2
-  # D-He3-p
-  # masses
-  m1 = md*mₑ
-  m2 = mHe3*mₑ
-  mmin = mp*mₑ
-  # charge numbers
-  z1 = 1
-  z2 = 2
-  zmin = 1
-
-  # concentrations and densities
-  # Fig 18 Cottrell 1993
-  n0 = Float64(@fetchfrom 1 _n0) #1.5e19# 5e19 # 1.7e19 # central electron density 3.6e19
-  B0 = Float64(@fetchfrom 1 _B0) #3.7 #2.07 = 2.8T * 2.96 m / 4m
-  # 2.23 T is 17MHz for deuterium cyclotron frequency
-  ξ = Float64(@fetchfrom 1 ximin) #1e-4#1.5e-4 # nα / ni = 1.5 x 10^-4
-  ξ2 = Float64(@fetchfrom 1 xi2) # 0.15
-  n2 = ξ2*n0
-  nmin = ξ*n0
-  n1 = (1/z1)*(n0-z2*n2-zmin*nmin) # 1 / (1.0 + 2*ξ)
-  @assert n0 ≈ z1*n1 + z2*n2 + zmin*nmin
-  density_weighted = n1*m1 + n2*m2 + nmin*mmin
-  Va = B0 / sqrt(LinearMaxwellVlasov.μ₀*density_weighted)
-
-  # temperatures and energies
-  Te = 1e3 * Float64(@fetchfrom 1 _Te)
-  T1 = Te # eV
-  T2 = T1 # eV
-  Emin = 1e6 * Float64(@fetchfrom 1 _Emin) # eV
-  #Emin = 14.68e6 # eV # 3.5e6
-  Ωe = cyclotronfrequency(B0, mₑ, ze)
-  Ω1 = cyclotronfrequency(B0, m1, z1)
-  Ωmin = cyclotronfrequency(B0, mmin, zmin)
-  Πe = plasmafrequency(n0, mₑ, ze)
-  Π1 = plasmafrequency(n1, m1, z1)
-  Πmin = plasmafrequency(nmin, mmin, zmin)
-  vthe = thermalspeed(Te, mₑ) # temperature, mass
-  vth1 = thermalspeed(T1, m1) # temperature, mass
-  vmin = thermalspeed(Emin, mmin) # energy in terms of eV (3.5e6)
-  if ξ2 != 0
-    Ω2 = cyclotronfrequency(B0, m2, z2)
-    Π2 = plasmafrequency(n2, m2, z2)
-    vth2 = thermalspeed(T2, m2) # temperature, mass
-    spec2_cold = ColdSpecies(Π2, Ω2)
-    spec2_warm = WarmSpecies(Π2, Ω2, vth2)
-    spec2_maxw = MaxwellianSpecies(Π2, Ω2, vth2, vth2)
-  else
-    Ω2 = 0
-    Π2 = 0
-    vth2 = 0
-    spec2_cold = 0
-    spec2_warm = 0
-    spec2_maxw = 0
-  end
-  # pitchanglecosine = cos(pitchangle)
-  # acos(pitchanglecosine) = pitchangle
-  pitchanglecosine = Float64(@fetchfrom 1 pitchanglecosine)
-  vα⊥ = vmin * sqrt(1 - pitchanglecosine^2) # perp speed
-  vαz = vmin * pitchanglecosine # parallel speed
-  vthermalfractionz = Float64(@fetchfrom 1 vthermalfractionz)
-  vthermalfraction⊥ = Float64(@fetchfrom 1 vthermalfraction⊥)
-  vαthz = vmin * vthermalfractionz
-  vαth⊥ = vmin * vthermalfraction⊥
-
-  electron_cold = ColdSpecies(Πe, Ωe)
-  electron_warm = WarmSpecies(Πe, Ωe, vthe)
-  electron_maxw = MaxwellianSpecies(Πe, Ωe, vthe, vthe)
-
-  spec1_cold = ColdSpecies(Π1, Ω1)
-  spec1_warm = WarmSpecies(Π1, Ω1, vth1)
-  spec1_maxw = MaxwellianSpecies(Π1, Ω1, vth1, vth1)
-
-  minspec_cold = ColdSpecies(Πmin, Ωmin)
-  minspec_maxw = MaxwellianSpecies(Πmin, Ωmin, vαthz, vαth⊥, vαz)
-  minspec_ringbeam = SeparableVelocitySpecies(Πmin, Ωmin,
-    FBeam(vαthz, vαz),
-    FRing(vαth⊥, vα⊥))
-  minspec_delta = SeparableVelocitySpecies(Πmin, Ωmin,
-    FParallelDiracDelta(vαz),
-    FPerpendicularDiracDelta(vα⊥))
-
-  if ξ2 != 0
-    Smmr = Plasma([electron_maxw, spec1_maxw, spec2_maxw, minspec_ringbeam]) #spec2_maxw change these for multiple ions
-    Smmd = Plasma([electron_maxw, spec1_maxw, spec2_maxw, minspec_delta]) #
-  else
-    Smmr = Plasma([electron_maxw, spec1_maxw, minspec_ringbeam]) #spec2_maxw change these for multiple ions
-    Smmd = Plasma([electron_maxw, spec1_maxw, minspec_delta]) #
-  end
-
-  w0 = abs(Ωmin)
-  k0 = w0 / abs(Va)
-
-  γmax = abs(Ωmin) * 1.5 # search larger maximum growth rates
-  γmin = -abs(Ωmin) * 0.075
-  function bounds(ω0)
-    lb = @SArray [ω0 * 0.5, γmin]
-    ub = @SArray [ω0 * 1.2, γmax]
-    return (lb, ub)
-  end
-
-  options = Options(memoiseparallel=false, memoiseperpendicular=true)
-
-  function solve_given_ks(K, objective!)
-    ω0 = fastzerobetamagnetoacousticfrequency(Va, K, Ω1)
-
-    lb, ub = bounds(ω0)
-
-    function boundify(f::T) where {T}
-      @inbounds isinbounds(x) = all(i->0 <= x[i] <= 1, eachindex(x))
-      maybeaddinf(x::U, addinf::Bool) where {U} = addinf ? x + U(Inf) : x
-      bounded(x) = maybeaddinf(f(x), !isinbounds(x))
-      return bounded
-    end
-
-    config = Configuration(K, options)
-
-#    ics = ((@SArray [ω0*0.8, γmax*0.9]),
-#           (@SArray [ω0*0.9, γmax*0.7]),
-#           (@SArray [ω0*0.9, γmax*0.3]),
-#           (@SArray [ω0*1.0, γmax*0.1]))
-    ics = ((@SArray [ω0*0.8, γmax*0.9]), # adjust initial conditions
-           (@SArray [ω0*0.9, γmax*0.7]), # to look in the right growth
-           (@SArray [ω0*0.9, γmax*0.1]), # rate range
-           (@SArray [ω0*1.0, γmax*0.01]))
-
-    function unitobjective!(c, x::T) where {T}
-      return objective!(c,
-        T([x[i] * (ub[i] - lb[i]) + lb[i] for i in eachindex(x)]))
-    end
-    unitobjectivex! = x -> unitobjective!(config, x)
-    boundedunitobjective! = boundify(unitobjectivex!)
-    xtol_abs = w0 .* (@SArray [1e-4, 1e-5]) ./ (ub .- lb)
-
-    innersolutions = []
-    @elapsed for ic ∈ ics
-      @assert all(i->lb[i] <= ic[i] <= ub[i], eachindex(ic))
-#      neldermeadsol = WindingNelderMead.optimise(
-#        boundedunitobjective!, SArray((ic .- lb) ./ (ub .- lb)),
-#        1.1e-2 * (@SArray ones(2)); stopval=1e-15, timelimit=30,
-#        maxiters=200, ftol_rel=0, ftol_abs=0, xtol_rel=0, xtol_abs=xtol_abs)
-#      simplex, windingnumber, returncode, numiterations = neldermeadsol
-#      if (windingnumber == 1 && returncode == :XTOL_REACHED)# || returncode == :STOPVAL_REACHED
-#        c = deepcopy(config)
-#        minimiser = if windingnumber == 0
-#          WindingNelderMead.position(WindingNelderMead.bestvertex(simplex))
-#        else
-#          WindingNelderMead.centre(simplex)
-#        end
-#        unitobjective!(c, minimiser)
-#        return c
-#      end
-      try
-        nlsolution = nlsolve(x->reim(boundedunitobjective!(x)),
-                             MArray((ic .- lb) ./ (ub .- lb)),
-                             xtol=1e-8, factor=0.1)
-        if nlsolution.x_converged || nlsolution.f_converged
-          c = deepcopy(config)
-          objective!(c, scaleup(lb, ub, nlsolution.zero))
-          push!(innersolutions, c)
-        end
-      catch
-      end
-    end
-    return innersolutions
-  end
-
-  scaleup(lb, ub, x) = (x .* (ub .- lb) .+ lb)
-
-  function f2Dω!(config::Configuration, x::AbstractArray, plasma, cache)
-    config.frequency = Complex(x[1], x[2])
-    return det(tensor(plasma, config, cache))
-  end
-
-  kparamax = Float64(@fetchfrom 1 _kparamax)
-  kperpmax = Float64(@fetchfrom 1 _kperpmax)
-
-  function findsolutions(plasma)
-    ngridpoints = Int(@fetchfrom 1 _ngridpoints)
-    kzs = range(-kparamax, stop=kparamax, length=ngridpoints) * k0
-    k⊥s = range(0.0, stop=kperpmax, length=ngridpoints) * k0
-
-    # change order for better distributed scheduling
-    k⊥s = shuffle(vcat([k⊥s[i:nprocs():end] for i ∈ 1:nprocs()]...))
-    @assert length(k⊥s) == ngridpoints
-    solutions = @sync @showprogress @distributed (vcat) for k⊥ ∈ k⊥s
-      cache = Cache()
-      objective! = @closure (C, x) -> f2Dω!(C, x, plasma, cache)
-      innersolutions = Vector()
-      for (ikz, kz) ∈ enumerate(kzs)
-        K = Wavenumber(parallel=kz, perpendicular=k⊥)
-        output = solve_given_ks(K, objective!)
-        isnothing(output) && continue
-        push!(innersolutions, output...)
-      end
-      innersolutions
-    end
-    return solutions
-  end
-end #@everywhere
-
-"Select the largest growth rates if multiple solutions found for a wavenumber"
-function selectlargeestgrowthrate(sols)
-    imagfreq(s) = imag(s.frequency)
-    d = Dict{Any, Vector{eltype(sols)}}()
-    for sol in sols
-      if haskey(d, sol.wavenumber)
-        push!(d[sol.wavenumber], sol)
-      else
-        d[sol.wavenumber] = [sol]
-      end
-    end
-    output = Vector()
-    sizehint!(output, length(sols))
-    for (_, ss) in d
-      push!(output, ss[findmax(map(imagfreq, ss))[2]])
-    end
-    return output
-end
-
-function selectpropagationrange(sols, lowangle=0, highangle=180)
-  function propangle(s)
-    kz = para(s.wavenumber)
-    k⊥ = perp(s.wavenumber)
-    θ = atan.(k⊥, kz) * 180 / pi
-  end
-  output = Vector()
-  for s in sols
-    (lowangle <= propangle(s) <= highangle) || continue
-    push!(output, s)
-  end
-  return output
-end
 
 Plots.gr() # .pyplot()
 function make2d(rowedges, coledges, rowvals, colvals, vals)
@@ -644,15 +366,8 @@ end
 
 
 if true#false
-  @time plasmasols = findsolutions(Smmr)#d
-#  plasmasols = selectlargeestgrowthrate(plasmasols)
-  mkpath(dirpath)
-  @save "$dirpath/solutions2D.jld" filecontents plasmasols w0 k0
-  @time plotit(plasmasols)
   rmprocs(nprocsadded)
-else
-  rmprocs(nprocsadded)
-  @load "$dirpath/solutions2D.jld" filecontents plasmasols w0 k0
+  @load "$dirpath/solutions2D_.jld" filecontents plasmasols w0 k0
   @time plotit(plasmasols)
 end
 
