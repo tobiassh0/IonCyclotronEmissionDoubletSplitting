13796ff4f94a4737f938359c57797afd4ffe998e
Tobias Slade-Harajda <tobias.slade-harajda@warwick.ac.uk>
2024-02-08 17:33:28 +0000

    finalised python loading and plotting

--- paralleltest.py
+++ paralleltest.py
@@ -10,7 +10,7 @@ def func(i,mult):
 mult = 2
 arr = np.arange(0,10,1)
 pool = mp.Pool(mp.cpu_count())
-x = partial(func,mult=mult)
+x = partial(func,mult=mult) # keeps arg "mult" as a constant in parallel loop over i
 print(x)
 res = np.array(pool.map_async(x,arr).get(99999))
 m1,m2=np.split(res,2,axis=1)
